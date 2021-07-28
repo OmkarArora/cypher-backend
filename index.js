b95d339d7839a6904644e106396892b1b3e0ffc6
Omkar Arora <aroraomkar@gmail.com>
2021-07-28 16:05:03 +0530

    chore: express server setup

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Connected to Cypher Quiz server");
+});
+
+// 404 Handler
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: "Route not found" });
+});
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+  console.log("SERVER STARTED on port: ", PORT);
+});
